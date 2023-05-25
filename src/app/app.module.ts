a8cf31083fee7613a78de4f3a9a14a143509103a
naim <naimullah.1400@gmail.com>
2023-05-25 13:24:02 +0430

    mini project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { ArticlesModule } from './articles/articles.module';
+import { StaticpagesModule } from './staticpages/staticpages.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    PagenotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ArticlesModule,
+    StaticpagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
